49773ac1ad5cb248b9aa1944f17d17938bce4907
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-05-18 16:37:32 -0700

    adds fyne to app

--- bible.go
+++ bible.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	a := app.New()
+
+	w := a.NewWindow("Bible App")
+	w.SetContent(widget.NewVBox(
+		widget.NewLabel("Check it out Joel! \n On my to catch up with you!"),
+		widget.NewButton("Quit", func() {
+			a.Quit()
+		}),
+	))
+
+	w.ShowAndRun()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module book.com/user/bible
 
 go 1.14
+
+require fyne.io/fyne v1.2.4
