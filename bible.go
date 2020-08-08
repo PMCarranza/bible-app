f8fcdb3c44fca69e8b9e8c79dee46c51b28602b1
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-08-08 08:46:33 -0700

    adds test text and window to app

--- bible.go
+++ bible.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"image/color"
+
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/canvas"
+	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 )
+
+func cell() fyne.CanvasObject {
+	cell := canvas.NewRectangle(&color.Black)
+	cell.SetMinSize(fyne.NewSize(30, 30))
+	return cell
+}
 
 func main() {
 	a := app.New()
 
 	w := a.NewWindow("Bible App")
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Check it out Joel! \n On my to catch up with you!"),
-		widget.NewButton("Quit", func() {
-			a.Quit()
-		}),
-	))
 
+	top := cell()
+	bottom := cell()
+	left := cell()
+	right := cell()
+
+	middle := widget.NewLabelWithStyle("test\ntest\ntest\ntest", fyne.TextAlignLeading, fyne.TextStyle{})
+
+	borderLayout := layout.NewBorderLayout(top, bottom, left, right)
+
+	pageLayout := fyne.NewContainerWithLayout(borderLayout, top, bottom, left, right, middle)
+	w.SetContent(pageLayout)
+
+	w.Resize(fyne.NewSize(600, 600))
+	w.SetFixedSize(false)
 	w.ShowAndRun()
 }
