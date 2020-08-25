ce7a1658ad85b05e3718b3b7b68534227a9d285b
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2020-08-24 17:04:01 -0700

    adds color to main background

--- bible.go
+++ bible.go
@@ -11,9 +11,15 @@ import (
 )
 
 func cell() fyne.CanvasObject {
-	cell := canvas.NewRectangle(&color.Black)
+	cell := canvas.NewRectangle(&color.Transparent)
 	cell.SetMinSize(fyne.NewSize(30, 30))
 	return cell
+}
+
+func fondo() fyne.CanvasObject {
+	fondo := canvas.NewRectangle(&color.RGBA{147, 220, 245, 5})
+	fondo.SetMinSize(fyne.NewSize(120, 120))
+	return fondo
 }
 
 func main() {
@@ -21,16 +27,17 @@ func main() {
 
 	w := a.NewWindow("Bible App")
 
+	underside := fondo()
+
 	top := cell()
 	bottom := cell()
 	left := cell()
 	right := cell()
 
-	middle := widget.NewLabelWithStyle("test\ntest\ntest\ntest", fyne.TextAlignLeading, fyne.TextStyle{})
-
+	middle := widget.NewLabelWithStyle("test\ntest\ntest\ntest", fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
 	borderLayout := layout.NewBorderLayout(top, bottom, left, right)
 
-	pageLayout := fyne.NewContainerWithLayout(borderLayout, top, bottom, left, right, middle)
+	pageLayout := fyne.NewContainerWithLayout(borderLayout, top, bottom, left, right, underside, middle)
 	w.SetContent(pageLayout)
 
 	w.Resize(fyne.NewSize(600, 600))
